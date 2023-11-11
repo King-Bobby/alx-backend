73262ba1e728150c998590edd57752297c88a91e
King-Bobby <noelbrowndboss@gmail.com>
2023-11-11 22:05:28 +0000

    ...

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for a basic Flask app
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Index route that renders the index.html template
+
+    Returns:
+        str: Rendered HTML content
+    """
+    return render_template('0-index.html',
+                           title='Welcome to Holberton',
+                           header='Hello world')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
